3c85a86cd8fc94da4c943ef43e8200ca4b88df1a
yaroslav <2005sparta22@gmail.com>
2024-06-17 21:19:36 +0300

    Домашка 3.1. Програма повинна перенести останній елемент на початок списку, послідовність списку незмінні.

--- lesson_3_1.py
+++ lesson_3_1.py
@@ -1,4 +1,7 @@
 list_1 = [12, 3, 4, 10]
-list_2 = list_1.pop(-1)
-list_1.insert(0, 10)
-print(list_1)
+if len(list_1) >= 1:
+    list_2 = list_1.pop()
+    list_1.insert(0, list_2)
+    print(list_1)
+else:
+    print([])
